e2709e55274fa70d897ab8ae5b3a14fec8ebd5bf
rsjhon90 <rsjhony@hotmail.com>
2020-10-19 12:23:47 -0300

    Database set: Migrations
    Padrões a ser seguido para criação, alteração e exclusão de dados.

---
+++ backend/src/database/migrations/1603118501279-create_orphanages.ts
@@ -0,0 +1,56 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createOrphanages1603118501279 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      //realizar criações e alterações no banco de dados
+      await queryRunner.createTable(new Table ({
+        name: 'orphanages',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'latitude',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'about',
+            type: 'text',
+          },
+          {
+            name: 'instructions',
+            type: 'text',
+          },
+          {
+            name: 'open_on_weekends',
+            type: 'boolean',
+            default: 'false',
+          },
+        ]
+      }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      //desfazer as alterações no método up
+      await queryRunner.dropTable('orphanages');
+    }
+
+}
