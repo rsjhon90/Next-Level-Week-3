13c27a8e837e94db82d2c8cac33d1dc896a208b3
rsjhon90 <rsjhony@hotmail.com>
2020-10-19 10:00:21 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 //express é um framework simples e eficaz para REQ e RES
+//todo backend automaticamente trabalha em requisição e resposta (request, reponse)
 
 const app = express();
 
-app.get('/users', () => {
-    console.log('teste');
-})
+app.get('/users', (request, response) => {
+    return response.json({ message: 'Hello World'});
+});
 
 app.listen(3333);
 //localhost/3333
