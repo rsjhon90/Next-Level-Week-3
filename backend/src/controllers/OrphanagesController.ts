62e72335ea1b7f7b37b6b797ab75736328a03ac9
rsjhon90 <rsjhony@hotmail.com>
2020-10-20 12:40:21 -0300

    Database set: upload images 2

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -22,6 +22,8 @@ export default {
   },
 
   async create(request: Request, response: Response){
+    // console.log(request.files);
+
     const {
       name,
       latitude,
@@ -33,6 +35,13 @@ export default {
     } = request.body;
   
     const orphanagesRepository = getRepository(Orphanage);
+
+    const requestImages = request.files as Express.Multer.File[];
+
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    });
+
   
     const orphanage = orphanagesRepository.create({
       name,
@@ -42,6 +51,7 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
+      images
     });
   
     await orphanagesRepository.save(orphanage);
